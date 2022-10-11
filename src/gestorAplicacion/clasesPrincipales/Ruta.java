1d1b1756c277e003bb9b71cba531b6d57680f3fc
Lema25 <56377966+Lema25@users.noreply.github.com>
2022-10-11 09:43:23 -0500

    Update Ruta.java

--- src/gestorAplicacion/clasesPrincipales/Ruta.java
+++ src/gestorAplicacion/clasesPrincipales/Ruta.java
@@ -1,5 +1,34 @@
 package gestorAplicacion.clasesPrincipales;
 
 public class Ruta {
-
+	private String partida;
+	private String[] paradas;
+	private String destino;
+	
+	public Ruta(String partida, String[] paradas, String destino) {
+		this.partida = partida;
+		this.paradas = paradas;
+		this.destino = destino;
+		
+	}
+	
+	public String[] getParadas() {
+		return paradas;}
+	public void setParadas(String[] paradas) {
+		this.paradas = paradas;}
+	
+	public String getPartida() {
+		return partida;}
+	public void setPartida(String partida) {
+		this.partida = partida;}
+	
+	public String getDestino() {
+		return destino;}
+	public void setParadas(String destino) {
+		this.destino = destino;}
+	
+	public String crearRuta() {
+		
+		return ;
+	}
 }
