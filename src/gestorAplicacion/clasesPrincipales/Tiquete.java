104a66378240bd71874eb7ff6b69680f09d04f25
Lema25 <56377966+Lema25@users.noreply.github.com>
2022-10-11 09:43:58 -0500

    Update Tiquete.java

--- src/gestorAplicacion/clasesPrincipales/Tiquete.java
+++ src/gestorAplicacion/clasesPrincipales/Tiquete.java
@@ -1,5 +1,24 @@
 package gestorAplicacion.clasesPrincipales;
 
 public class Tiquete {
-
+	private String codigo;
+	private Viaje viaje;
+	private Ruta ruta;
+	
+	public Tiquete (String codigo, Viaje viaje, Ruta ruta) {
+		this.codigo = codigo;
+		this.viaje = viaje;
+		this.ruta = ruta;
+		
+	}
+	
+	public String getCodigo() {
+		return codigo;}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;}
+	
+	public String consultarTiquete() {
+		
+		return ;
+	}
 }
