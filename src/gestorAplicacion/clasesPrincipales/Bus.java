1beb3aa385bb5feb07f2c764f466820654b84470
niperezv <47955066+niperezv@users.noreply.github.com>
2022-10-10 22:27:36 -0500

    NicolasAV1

--- src/gestorAplicacion/clasesPrincipales/Bus.java
+++ src/gestorAplicacion/clasesPrincipales/Bus.java
@@ -1,5 +1,19 @@
 package gestorAplicacion.clasesPrincipales;
+import java.util.ArrayList;
 
 public class Bus {
-
+	String modelo;
+	String placa;
+	int capacidad;
+	//atributo_interseccion
+	private ArrayList<Empresa> empresa = new ArrayList<Empresa>();
+	//get&set_Empresa
+	public ArrayList<Empresa> getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(ArrayList<Empresa> empresa) {
+		this.empresa = empresa;
+	}
+	/**Metodo adquirir servicio raro**/
 }
+
--- src/gestorAplicacion/clasesPrincipales/Empresa.java
+++ src/gestorAplicacion/clasesPrincipales/Empresa.java
@@ -1,5 +1,49 @@
 package gestorAplicacion.clasesPrincipales;
+import java.util.ArrayList;
 
 public class Empresa {
+	/**atributo conductores contendra una lista con los conductores de la empresa cardinalidad[0..*]**/
+	private ArrayList<Conductor> conductores = new ArrayList<Conductor>();
+	/**atributo viajes contendra una lista con los viajes de la empresa cardinalidad[0..*]**/
+	private ArrayList<Viaje> viajes = new ArrayList<Viaje>();
+	/**atributo buses contendra una lista con los buses de la empresa cardinalidad[0..*]**/
+	private ArrayList<Bus> buses = new ArrayList<Bus>();
+	/**suposicion1:encapsulamiento,privado**/
+	/**DUDA:el atributo interseccion no se crea por atributos?**/
+	/**Discusion:Que queremos con viajes? guardar todas las instancias de tipo viaje?**/
+	
+	//Get&Set_Conductores
+	public ArrayList<Conductor> getConductores() {
+		return conductores;
+	}
+	public void setConductores(ArrayList<Conductor> conductores) {
+		this.conductores = conductores;
+	}
+	//Get&Set_Viajes
+	public ArrayList<Viaje> getViajes() {
+		return viajes;
+	}
 
+	public void setViajes(ArrayList<Viaje> viajes) {
+		this.viajes = viajes;
+	}
+
+	//Get&Set_Buses
+	public ArrayList<Bus> getBuses() {
+		return buses;
+	}
+
+	public void setBuses(ArrayList<Bus> buses) {
+		this.buses = buses;
+	}
+	//Metodo_crearViaje
+	/**deberia re-considerarse el nombre**/
+	public void crearViaje(Viaje viaje) {
+		viajes.add(viaje);
+		}
+	//Metodo_CancelarViaje
+	/**si es un metodo que borre la instancia de viaje o cambie un valor como estadoviaje podria ser
+	 * un metodo de la clase viaje
+	 */
+	
 }
