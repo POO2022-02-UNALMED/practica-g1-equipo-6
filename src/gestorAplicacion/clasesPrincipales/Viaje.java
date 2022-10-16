252338ba8305cba77eb9da9181d0903a605d913b
camilobelloHZ <cbelloh@unal.edu.co>
2022-10-16 01:02:56 -0500

    4

--- src/gestorAplicacion/clasesPrincipales/Viaje.java
+++ src/gestorAplicacion/clasesPrincipales/Viaje.java
@@ -1,39 +1,54 @@
 package gestorAplicacion.clasesPrincipales;
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.io.Serializable;
+import gestorAplicacion.clasesPrincipales.Ruta;
 
-
-public class Viaje {
+public class Viaje implements Serializable{
+	
+	private static final long serialVersionUID= 8L;
+	
 	private String hora_inicio;
 	private String hora_llegada;
 	//private String origen;
 	private Ruta ruta;
-	//private String destino;
-	private String fecha;
+	private LocalDate fecha;
 	private int precio;
 	private Conductor conductor;
 	private Bus bus;
-	private Boolean enViaje;
+	private boolean enViaje;
+	private static ArrayList<Viaje> viajes= new ArrayList<Viaje>();
+	private static ArrayList<String> lugares= new ArrayList<String>();
+	private ArrayList<Tiquete> tiquetesTodos;
 
 //contructor
 
-	public Viaje(String hora_inicio, String hora_llegada, String fecha, int precio,Ruta ruta, Conductor conductor,
+	public Viaje(String hora_inicio, String hora_llegada, LocalDate fecha, int precio, Ruta ruta, Conductor conductor,
 	Bus bus, Boolean enViaje ) {
 		this.hora_inicio= hora_inicio;
 		this.hora_llegada= hora_llegada;
-		//this.fecha= fecha;
+		this.fecha= fecha;
 		//this.origen= 
-		this.ruta= ruta;
+		//this.origen= this.ruta.getOrigen();
+		//this.destino= this.ruta.getDestino();
+		this.ruta= ruta;//Ruta debe crear el destino y el origen sino debe tenerlas el viaje 
 		this.precio= precio;
 		this.conductor= conductor;
 		this.enViaje = true;
 		this.bus= bus;
+		this.tiquetesTodos= new ArrayList<>();
 		
-		int j=0;
+		
 		for (String sillaEnVehiculo: this.bus.getSillas()) {
-			this.allTiquetes.add(new Tiquete(j, null, sillaEnVehiculo, this, tipoSilla, fechaViaje));
-			j++;
-			}
+			int genId = this.bus.getSillas().indexOf(sillaEnVehiculo);
+			this.tiquetesTodos.add(new Tiquete(genId, null, sillaEnVehiculo, this,10000, fecha));//Valor tengo duda
+			
+		}
+		viajes.add(this);
+	}
 	
-		public Viaje(){
+		
+	public Viaje(){
 		}
 
 		
@@ -50,10 +65,10 @@ public class Viaje {
 	public void setHora_llegada(String hora_llegada) {
 		this.hora_llegada = hora_llegada;
 	}
-	public String getFecha() {
+	public LocalDate getFecha() {
 		return fecha;
 	}
-	public void setFecha(String fecha) {
+	public void setFecha(LocalDate fecha) {
 		this.fecha = fecha;
 	}
 	public int getPrecio() {
@@ -74,10 +89,10 @@ public class Viaje {
 	public void setBus(Bus bus) {
 		this.bus = bus;
 	}
-	public Boolean getEnViaje() {
+	public boolean getEnViaje() {
 		return enViaje;
 	}
-	public void setEnViaje(Boolean enViaje) {
+	public void setEnViaje(boolean enViaje) {
 		this.enViaje = enViaje;
 	}
 
@@ -89,12 +104,52 @@ public class Viaje {
 
 	/*public int asignarsilla() {
 		return c;
-	}
+	}*/
 
 	public Tiquete generarTiquete() {	
 	
 	}
+	
+	public ArrayList<Tiquete> tiquetesDisponibles(){
+		ArrayList<Tiquete> tiqueteFinal = new ArrayList<>();
+		for(Tiquete tiquete : tiquetesTodos){
+			if(tiquete.getEstado() == false){
+				tiqueteFinal.add(tiquete);
+			}
+		}
+		return tiqueteFinal;
+	}
 
+	public void eliminarViaje(){
+		Viaje.viajes.remove(this);
+	}
+	
+	
+	
+	//
+	@Override
+	public String toString() {
+		return "Viaje{" +
+				", origen=" + origen +//Rutas debe crear getOrigen donde saque el origen de las rutas 
+				", destino=" + destino +
+				", fechaViaje=" + fecha +
+				'}';
+	}
+	
+	
+	public static ArrayList<String> getLugares(){
+		for( Viaje viajes: Viaje.getViajes()) {
+			if(!lugares.contains(viajes.getOrigen())) {
+				lugares.add(viajes.getOrigen());
+			}
+		}
+		for (Viaje viajes: Viaje.getViajes()) {
+			if(!lugares.contains(viajes.getDestino())) {
+				lugares.add(viajes.getDestino());;
+			}
+		}return lugares;
+	}
+/*
 	public Tiquete eliminarTiquete() {
 	
 	} */
