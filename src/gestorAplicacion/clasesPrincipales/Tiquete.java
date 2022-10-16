f111f17dac442cea7d1a56803b22eb88e3871583
Lema25 <aflemag@unal.edu.co>
2022-10-15 19:23:24 -0500

    Update Tiquete.java

--- src/gestorAplicacion/clasesPrincipales/Tiquete.java
+++ src/gestorAplicacion/clasesPrincipales/Tiquete.java
@@ -1,24 +1,86 @@
 package gestorAplicacion.clasesPrincipales;
 
-public class Tiquete {
-	private String codigo;
-	private Viaje viaje;
-	private Ruta ruta;
-	
-	public Tiquete (String codigo, Viaje viaje, Ruta ruta) {
-		this.codigo = codigo;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Tiquete implements Serializable {
+		
+		private static final long serialVersionUID = 7L;
+		private int codigo;
+		protected Pasajero pasajero;
+		protected Bus sillaTiquete;
+		protected Viaje viaje;
+		protected int valor;
+		protected LocalDate fechaCompra;
+		private boolean estado;
+		private static ArrayList<Tiquete> tiquetes;
+
+		static {
+			tiquetes = new ArrayList<Tiquete>();
+		}
+
+	public  Tiquete(){}
+
+	public Tiquete(Bus sillaTiquete, Viaje viaje, int valor) {
+		this.sillaTiquete = sillaTiquete;
 		this.viaje = viaje;
-		this.ruta = ruta;
-		
+		this.estado = false;
+		this.valor = valor;
 	}
-	
-	public String getCodigo() {
-		return codigo;}
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;}
-	
-	public String consultarTiquete() {
-		
-		return ;
+
+	public Tiquete(int codigo, Pasajero pasajero, Bus sillaTiquete, Viaje viaje, int valor, LocalDate fechaCompra) {
+		this.codigo = codigo;
+		this.pasajero = pasajero;
+		this.sillaTiquete = sillaTiquete;
+		this.viaje = viaje;
+		this.valor = valor;
+		this.estado = false;
+		this.fechaCompra = fechaCompra;
+		Tiquete.tiquetes.add(this);
+	}
+
+
+	public Bus getSillaTiquete() {
+		return sillaTiquete;}
+
+	public Viaje getViaje() {
+		return viaje;	}
+
+	public Pasajero getUsuario() {
+		return pasajero;}
+
+	public boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public void setComprador(Pasajero comprador) {
+		this.pasajero = comprador;}
+
+	public int getValor() {	
+		return valor;}
+
+	public Pasajero getComprador() {	
+		return pasajero;}
+
+	public void setFechaCompra(LocalDate fechaCompra) {	
+		this.fechaCompra = fechaCompra;	}
+
+	public static ArrayList<Tiquete> getTiquetes() {
+		return tiquetes;	}
+
+	@Override
+	public String toString() {
+		return "Tiquete = " +"ID : " + codigo + ", SILLA :" + sillaTiquete + "\n" +
+				"	VIAJE =" + viaje + 
+				", valor : " + valor +
+				", fechaCompra : " + fechaCompra;
 	}
 }
+
+
+
+
