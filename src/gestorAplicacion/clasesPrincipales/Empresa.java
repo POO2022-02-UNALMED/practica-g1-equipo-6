107ad40486aac03ade1cb55f6bd1d14c17ec9d3b
niperezv <47955066+niperezv@users.noreply.github.com>
2022-10-16 17:43:13 -0500

    Update Empresa.java

--- src/gestorAplicacion/clasesPrincipales/Empresa.java
+++ src/gestorAplicacion/clasesPrincipales/Empresa.java
@@ -1,21 +1,28 @@
 package gestorAplicacion.clasesPrincipales;
-
-import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Empresa  implements Serializable {
-	
-	private static final long serialVersionUID = 3L;
-	
-	/**atributo conductores contendra una lista con los conductores de la empresa cardinalidad[0..*]**/
+public class Empresa {
+	//atributo conductores contendra una lista con los conductores de la empresa cardinalidad[0..*]
 	private ArrayList<Conductor> conductores = new ArrayList<Conductor>();
-	/**atributo viajes contendra una lista con los viajes de la empresa cardinalidad[0..*]**/
+	//atributo viajes contendra una lista con los viajes de la empresa cardinalidad[0..*]
 	private ArrayList<Viaje> viajes = new ArrayList<Viaje>();
-	/**atributo buses contendra una lista con los buses de la empresa cardinalidad[0..*]**/
+	//atributo buses contendra una lista con los buses de la empresa cardinalidad[0..*]
 	private ArrayList<Bus> buses = new ArrayList<Bus>();
+	
 	/**suposicion1:encapsulamiento,privado**/
 	/**DUDA:el atributo interseccion no se crea por atributos?**/
 	/**Discusion:Que queremos con viajes? guardar todas las instancias de tipo viaje?**/
+	
+	//Constructores
+	Empresa(Conductor conductor, Viaje viaje, Bus bus){
+		this.conductores.add(conductor);
+		this.viajes.add(viaje);
+		this.buses.add(bus);
+	}
+	
+	Empresa(){
+		
+	}
 	
 	//Get&Set_Conductores
 	public ArrayList<Conductor> getConductores() {
@@ -42,11 +49,18 @@ public class Empresa  implements Serializable {
 		this.buses = buses;
 	}
 	//Metodo_crearViaje
-	/**deberia re-considerarse el nombre**/
-	public void crearViaje(Viaje viaje) {
-		viajes.add(viaje);
+	public void crearViaje(){
+		//Viaje.crearViaje();
+		/**en viaje*/
+		/**static Viaje crearViaje(params) {
+			return new Viaje(params)
+		}**/
+		//se puede invocar un constructor dentro de un método?
 		}
 	//Metodo_CancelarViaje
+	public void CancelarViaje(Viaje viaje) {
+		viaje=null;
+	}
 	/**si es un metodo que borre la instancia de viaje o cambie un valor como estadoviaje podria ser
 	 * un metodo de la clase viaje
 	 */
