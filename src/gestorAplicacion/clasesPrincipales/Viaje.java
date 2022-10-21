c491db909061cb08b21b19a57e8c4ec2cdd51640
camilobelloHZ <cbelloh@unal.edu.co>
2022-10-20 22:05:08 -0500

    prueba

--- src/gestorAplicacion/clasesPrincipales/Viaje.java
+++ src/gestorAplicacion/clasesPrincipales/Viaje.java
@@ -137,8 +137,8 @@ public class Viaje implements Serializable{
 	}*/
 
 	
-	public static Tiquete generarTiquete(int codigo, Pasajero pasajero, Bus sillaTiquete, Viaje viaje, int valor, LocalDate fechaCompra) {
-		return new Tiquete(codigo, pasajero, sillaTiquete, viaje, valor, fechaCompra);
+	public static Tiquete generarTiquete(int codigo, Pasajero pasajero, String silla, Viaje viaje, int valor, LocalDate fechaCompra) {
+		return new Tiquete(codigo, pasajero, silla, viaje, valor, fechaCompra);
 	}
 	
 	public static ArrayList<Viaje> getViajes() {
