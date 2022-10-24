4cc5075ccd80769b837c3cc50ce1be2a13b7cfa7
daestradam <daestradam@unal.edu.co>
2022-10-24 16:15:26 -0500

    Create AdministrarViaje.java

---
+++ src/uiMain/funcionalidades/AdministrarViaje.java
@@ -0,0 +1,29 @@
+package uiMain.funcionalidades;
+
+import java.util.Scanner;
+
+public class AdministrarViaje {
+	static Scanner sc = new Scanner(System.in);
+    public static void showMenuViaje() {
+        System.out.println(" ");
+        System.out.println("----- SISTEMA DE ADMINISTRACION DE VIAJES -----");
+        
+        System.out.println("¿Que operacion desea realizar?");
+        System.out.println("[1] Agregar un nuevo viaje");
+        System.out.println("[2] Modificar un viaje");
+        System.out.println("[3] Eiminar un viaje");
+        int opcion = sc.nextInt();
+        switch (opcion) {
+        case 1:
+        	AdministrarViaje.;
+            break;
+        case 2:
+        	AdministrarViaje.;   
+        	break;
+        case 3:
+        	AdministrarViaje.;
+        	break;
+        }
+        
+    }
+}
