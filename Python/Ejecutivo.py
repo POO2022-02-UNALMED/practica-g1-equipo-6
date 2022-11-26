f518102ade00aa0bd4e6b075da8049af1ca37152
niperezv <47955066+niperezv@users.noreply.github.com>
2022-11-25 21:35:16 -0500

    {Nico}UpClass

---
+++ Python/Ejecutivo.py
@@ -0,0 +1,35 @@
+﻿from Bus import Bus
+from Viaje import Viaje
+
+class Ejecutivo(Bus):
+
+    def __init__(self,placa,conductor,sillas,precioKm=500,codigo=150,capacidad=26):
+        ejecutivos = []
+        super().__init__(placa,conductor,sillas)
+        Ejecutivo.ejecutivos.append(self)
+
+    #get&set capacidad
+    def getCapacidad(self):
+        return self._capacidad
+    def setCapacidad(self, capacidad):
+        self._capacidad = capacidad
+
+    #get_precio
+    def getPrecioKm(self):
+        return self._precioKm
+    def setPrecio(self, precioKm):
+        self._precioKm = precioKm
+
+    #get&set codigo
+    def getCodigo(self):
+        return self.codigo
+    def setCodigo(self, codigo):
+        self.codigo = codigo
+
+    #Metodo_sillasdisponibles
+    def sillasDisponibles(self):
+        for tiquete in Viaje.getTiquetesTodos():
+            if tiquete.getEstado() == False:
+                print("la silla: "+tiquete.getSillaTiquete()+"esta disponible")
+    def sillaTiquete(self, tiquete):
+        return tiquete.getSillaTiquete()
---
+++ Python/TecnoVans.py
@@ -0,0 +1,30 @@
+﻿from Bus import Bus
+from Viaje import Viaje
+
+class TecnoVans(Bus):
+
+    def __init__(self,placa,conductor,sillas,precioKm=800,codigo=200,capacidad=22):
+        tecnosV = []
+        super().__init__(placa,conductor,sillas)
+        TecnoVans.tecnosV.append(self)
+
+    #get&set capacidad
+    def getCapacidad(self):
+        return self._capacidad
+
+    #get&set PrecioKm
+    def getPrecioKm(self):
+        return self._precioKm
+
+    #get&set codigo
+    def getCodigo(self):
+        return self.codigo
+
+    #MetodoSillasDispo
+    def sillasDisponibles(self):
+        for tiquete in Viaje.getTiquetesTodos():
+            if tiquete.getEstado() == False:
+                print("la silla: "+tiquete.getSillaTiquete()+"esta disponible")
+            
+    def sillaTiquete(self, tiquete):
+        return tiquete.getSillaTiquete()
