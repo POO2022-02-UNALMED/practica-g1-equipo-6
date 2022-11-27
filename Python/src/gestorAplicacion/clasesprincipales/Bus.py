6ddae95798e3a1ba3733ef74909e08dd38a137d0
niperezv <47955066+niperezv@users.noreply.github.com>
2022-11-27 13:28:50 -0500

    Add files via upload

---
+++ Python/src/gestorAplicacion/clasesprincipales/Bus.py
@@ -0,0 +1,69 @@
+﻿class Bus:
+
+    #Constructor
+    def __init__(self,placa,conductor,sillas,km=0,precioKm=0,capacidad=0,codigo=0):
+        self.placa = placa
+        self.conductor = conductor
+        self._sillas = sillas
+        _buses = [] #Agregue este atributo valen
+        _sillaNoDisponibles = []
+
+
+    #get&set buses
+    @staticmethod
+    def getBuses():
+        return Bus._buses
+    @staticmethod
+    def setBuses(buses):
+        Bus._buses = buses
+
+    #get&set placa
+    def getPlaca(self):
+        return self.placa
+    def setPlaca(self, placa):
+        self.placa=placa
+
+    #get&set conductor
+    def getConductor(self):
+        return self.conductor
+    def setConductor(self, conductor):
+        self.conductor=conductor
+
+    #get&set sillas
+    def getSillas(self):
+        return self._sillas
+    def setSillas(self, sillas):
+        self._sillas = sillas
+
+    #get&set capacidad
+    def getCapacidad(self):
+        return self.capacidad
+    def setCapacidad(self, capacidad):
+        self.capacidad=capacidad
+
+    #get&set sillanodisponible
+    @staticmethod
+    def getSillaNoDisponibles():
+        return Bus._sillaNoDisponibles
+    @staticmethod
+    def setSillaNoDisponibles(sillaNoDisponibles):
+        Bus._sillaNoDisponibles.append(sillaNoDisponibles)
+
+    #get&set km
+    def getKm(self):
+        return self.km
+    def setKm(self, km):
+        self.km = km
+
+    #get&set precio
+    def getPrecioKm(self):
+        return self.precioKm
+    def setPrecioKm(self, precioKm):
+        self.precioKm = precioKm
+
+    #get&set precio
+    def getCodigo(self):
+        return self._codigo
+    def setCodigo(self, codigo):
+        self._codigo = codigo
+
---
+++ Python/src/gestorAplicacion/clasesprincipales/EuroVans.py
@@ -0,0 +1,30 @@
+﻿from Bus import Bus
+from Viaje import Viaje
+
+class EuroVans(Bus):
+
+    def __init__(self,placa,conductor,sillas,precioKm=1000,codigo=250,capacidad=14):
+        eurosV = []
+        super().__init__(placa,conductor,sillas)
+        EuroVans.eurosV.append(self)
+
+    #get&set capacidad
+    def getCapacidad(self):
+        return self._capacidad
+
+    #get&set precio   
+    def getPrecioKm(self):
+        return self._precioKm
+
+    #get&set codigo
+    def getCodigo(self):
+        return self.codigo
+
+    #Metodo_SillasDisponibles
+    def sillasDisponibles(self):
+        for tiquete in Viaje.getTiquetesTodos():
+            if tiquete.getEstado() == False:
+                print("la silla: "+tiquete.getSillaTiquete()+"esta disponible")
+    def sillaTiquete(self, tiquete):
+        return tiquete.getSillaTiquete()
+
