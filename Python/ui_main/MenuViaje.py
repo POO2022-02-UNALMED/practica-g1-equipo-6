d357813dc84c4ac9ae0b3f2e5f0c047da5297e67
Daniel Estrada <daestradam@unal.edu.co>
2022-11-29 15:46:55 -0500

    Update MenuViaje.py

--- Python/ui_main/MenuViaje.py
+++ Python/ui_main/MenuViaje.py
@@ -43,9 +43,65 @@ class MenuViaje(tk.Frame):
         btnModificarViaje.pack(fill="x", padx="5", pady="3")
         btnCancelarViaje.pack(fill="x", padx="5", pady="3")
 
-    def agregarViaje():
-        pass
-    def modificarViaje():
-        pass
-    def cancelarViaje():
-        pass
+    def agregarViaje(self):
+        self.master.limpiarFrame(self.main)
+        
+        entryIDVar = tk.StringVar()
+        entryHLlegadaVar = tk.StringVar()
+        entryHsalidaVar = tk.StringVar()
+        entryFrecuenciaVar = tk.StringVar()
+        entryCostoVar = tk.StringVar()
+
+        def submit():
+            ID = entryIDVar.get()
+            HLlegada = entryHLlegadaVar.get()
+            HSalida = entryHsalidaVar.get()
+            Frecuencia = entryHsalidaVar.get()
+            Costo = entryHsalidaVar.get()
+            validador = Validador()
+            validador.validarCampoVacio(ID, HLlegada, HSalida, Frecuencia, Costo)
+
+        lblTitulo = ttk.Label(self.main, text="Agregar Viaje", font=("Segoe UI", 20))
+        lblID = ttk.Label(self.main, text="ID del viaje")
+        entryID = ttk.Entry(self.main, textvariable=entryIDVar)
+        lblHLlegada = ttk.Label(self.main, text="Hora de inicio del viaje (HH:MM)")
+        entryHLlegada = ttk.Entry(self.main, textvariable=entryHLlegadaVar)
+        lblHSalida = ttk.Label(self.main, text="Hora de llegada al destino del viaje (HH:MM)")
+        entryHSalida = ttk.Entry(self.main, textvariable=entryHsalidaVar)
+        lblFrecuencia = ttk.Label(self.main, text="Frecuencia del viaje")
+        entryFrecuencia = ttk.Entry(self.main, textvariable=entryHsalidaVar)
+        lblCosto = ttk.Label(self.main, text="Costo del viaje")
+        entryCosto = ttk.Entry(self.main, textvariable=entryHsalidaVar)
+        
+        lblTitulo.pack(pady=(0, 20))
+        lblID.pack()
+        entryID.pack()
+        lblHLlegada.pack()
+        entryHLlegada.pack()
+        lblHSalida.pack()
+        entryHSalida.pack() 
+        lblFrecuencia.pack()
+        entryFrecuencia.pack() 
+        lblCosto.pack()
+        entryCosto.pack()       
+
+        btnSubmit = ttk.Button(
+                self.main, 
+                text="Submit", 
+                command=submit
+        )
+        btnSubmit.pack()
+
+    def modificarViaje(self):
+        self.master.limpiarFrame(self.main)
+        lblTitulo = ttk.Label(self.main, text="Modificar Viaje", font=("Segoe UI", 20))
+        lblTypo = ttk.Label(self.main, text="¿Que desea modificar?")
+        lblTitulo.pack(pady=(0, 20))
+        lblTypo.pack()
+        ttk.Radiobutton(self.main, text = "Fecha", value = 1).pack()
+        ttk.Radiobutton(self.main, text = "Precio", value = 2).pack()
+
+    def cancelarViaje(self):
+        self.master.limpiarFrame(self.main)
+        lblTitulo = ttk.Label(self.main, text="Eliminar Viaje", font=("Segoe UI", 20))
+        lblTitulo.pack(pady=(0, 20))
